8f601e02d23bbf6b0aeb3986f018862f7c9eba33
Muhammad Ibrahim <muhammadibrahim@Muhammads-MacBook-Air.local>
2021-02-24 22:14:38 -0600

    content done, styling up next

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
+import React, { useCallback, useState } from 'react'
 import './App.css';
 
-function App() {
+function Input(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <label>{props.label}</label>
+      <input type="text" value={props.text} onChange={(e) => props.handleChange(e.target.value)} />
+    </div>
+  );
+}
+
+function App(props) {
+  const[initialText, setInitialText] = useState("");
+  const[yearlyText, setYearlyText] = useState("");
+  const[growthText, setGrowthText] = useState("");
+  const[yearText, setYearText] = useState("");
+  const[resultText, setResultText] = useState("");
+  const calculate = useCallback(() => {
+    let total = Number(initialText);
+    for (let i = 0; i < Number(yearText); i++) {
+      total += Number(yearlyText) + (total + Number(yearlyText)) * Number(growthText) / 100;
+    }
+    setResultText(total);
+  });
+  return (
+    <div>
+      {resultText !== "" ? 
+      <h1>${resultText.toFixed(2)}</h1> 
+      : null}
+      <Input label="Initial Amount" text={initialText} handleChange={setInitialText}></Input>
+      <Input label="Yearly Contribution" text={yearlyText} handleChange={setYearlyText}></Input>
+      <Input label="Annual Growth Percentage" text={growthText} handleChange={setGrowthText}></Input>
+      <Input label="Number of Years" text={yearText} handleChange={setYearText}></Input>
+      <button onClick={() => calculate()}>Calculate Growth</button>
     </div>
   );
 }
